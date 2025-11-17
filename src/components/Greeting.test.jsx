a48eca60cc599d9fcc7fac050068ca1775676487
Ajin B <ajinb098@gmail.com>
2025-11-17 21:27:58 +0530

    Greeting Message

--- src/components/Greeting.test.jsx
+++ src/components/Greeting.test.jsx
@@ -6,7 +6,7 @@ import Greeting from './Greeting';
 // test(renders greeting with provided name) callback
 test('renders greeting with provided name', () => {
   render(<Greeting name="Test User" />);
-  expect(screen.getByText('Hello, Test User!')).toBeInTheDocument();
+  expect(screen.getByText('Hello Hi, Test User!')).toBeInTheDocument();
 });
 
 test('renders greeting with default name if no name is provided', () => {
